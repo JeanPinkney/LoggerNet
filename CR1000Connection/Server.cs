18f073fb585078759996fe1a45c629987852a5d2
Nicolas Hock Isaza <nhocki@gmail.com>
2012-10-24 14:41:57 -0400

    Move event arg classes to a new file

--- CR1000Connection/Server.cs
+++ CR1000Connection/Server.cs
@@ -4,32 +4,6 @@ using System.Text;
 
 namespace CR1000Connection
 {
-    public class ClockSyncArgs : EventArgs
-    {
-        public ClockSyncArgs(bool successful, CSIDATALOGGERLib.clock_outcome_type response_code, DateTime current_date)
-        {
-            this.successful     = successful;
-            this.current_date   = current_date;
-            this.response_code  = (int)response_code;
-        }
-        public bool successful;
-        public int response_code;
-        public DateTime current_date;
-    }
-
-    public class SentFileArgs : EventArgs
-    {
-        public SentFileArgs(bool successful, CSIDATALOGGERLib.prog_send_outcome_type response_code, string compile_result)
-        {
-            this.successful     = successful;
-            this.response_code  = (int) response_code;
-            this.compile_result = compile_result;
-        }
-        public bool successful;
-        public int response_code;
-        public string compile_result;
-    }
-
     public class Server
     {
         public delegate void ClockSyncHandler(object sender, ClockSyncArgs cs);
@@ -104,6 +78,7 @@ namespace CR1000Connection
                 dataLogger.programSendStart(programPath, programName);
             }
         }
+
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////// PRIVATE FUNCTIONS
